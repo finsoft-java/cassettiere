0d96e40d7e36b3e947c619603cba665953ca64be
Luca Vercelli <luca.vercelli.to@gmail.com>
2021-03-24 13:00:54 +0100

    docs

--- cassettiere/src/app/mat-edit-table/ColumnDefinition.ts
+++ cassettiere/src/app/mat-edit-table/ColumnDefinition.ts
@@ -4,12 +4,19 @@ import { Observable } from 'rxjs';
  * DataTable-like column definition structure
  */
 export interface ColumnDefinition<T> {
+  /** Titolo della colonna */
   title: string;
+  /** Nome dell'attributo dell'oggetto. Non obbligatorio se usi render. */
   data: string;
+  /** input type: text/number/date/hidden/select/... */
   type?: string;
+  /** width style property */
   width?: string;
-  render?: (data: any, row?: T, rowNum?: number, colNum?: number) => string | null; // Puoi usarlo per formattare le date
+  /** render function. Puoi usarlo per formattare le date */
+  render?: (data: any, row?: T, rowNum?: number, colNum?: number) => string | null;
+  /** select options */
   options?: LabelValue[];
+  /** funzione per caricare asincronamente le options */
   asyncOptions?: (row?: T) => Observable<LabelValue[]>;
 }
 
