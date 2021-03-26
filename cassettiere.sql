14aea12466571aafb65353fadcae09b5b4c7ed07
Luca Vercelli <luca.vercelli.to@gmail.com>
2021-03-26 18:03:24 +0100

    dati

--- cassettiere.sql
+++ cassettiere.sql
@@ -32,6 +32,18 @@ CREATE TABLE `aree` (
   `DESCRIZIONE` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dump dei dati per la tabella `aree`
+--
+
+INSERT INTO `aree` (`COD_AREA`, `DESCRIZIONE`) VALUES
+('A7', 'Cortile oratorio'),
+('E3', 'Edificio 3'),
+('E4', 'Edificio 4'),
+('M1', 'Capannone M1'),
+('M2', 'Capannone M2'),
+('M4', 'Magazzino 4');
+
 -- --------------------------------------------------------
 
 --
@@ -42,6 +54,14 @@ CREATE TABLE `operazioni` (
   `COD_OPERAZIONE` varchar(15) NOT NULL,
   `DESCRIZIONE` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dump dei dati per la tabella `operazioni`
+--
+
+INSERT INTO `operazioni` (`COD_OPERAZIONE`, `DESCRIZIONE`) VALUES
+('ESAURIMENTO', 'Segnalazione materiale in esaurimento'),
+('RABBOCCO', 'Segnalazione rabbocco materiale');
 
 -- --------------------------------------------------------
 
@@ -59,6 +79,13 @@ CREATE TABLE `storico_operazioni` (
   `TIMESTAMP` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dump dei dati per la tabella `storico_operazioni`
+--
+
+INSERT INTO `storico_operazioni` (`ID_OPERAZIONE`, `COD_UTENTE`, `COD_OPERAZIONE`, `COD_ARTICOLO`, `COD_UBICAZIONE`, `COD_AREA`, `TIMESTAMP`) VALUES
+(1, '1111', 'ESAURIMENTO', 'AAAA', 'UBI-M1-001', 'M1', '2021-03-26 16:12:10');
+
 -- --------------------------------------------------------
 
 --
@@ -72,6 +99,30 @@ CREATE TABLE `ubicazioni` (
   `COD_AREA` varchar(10) NOT NULL,
   `SEGNALAZIONE_ESAURIMENTO` varchar(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dump dei dati per la tabella `ubicazioni`
+--
+
+INSERT INTO `ubicazioni` (`COD_UBICAZIONE`, `COD_ARTICOLO_CONTENUTO`, `QUANTITA_PREVISTA`, `COD_AREA`, `SEGNALAZIONE_ESAURIMENTO`) VALUES
+('UBI-A7-001', 'AAAAA', 50, 'A7', 'N'),
+('UBI-A7-002', 'BBBBB', 50, 'A7', 'N'),
+('UBI-A7-003', 'CCCCC', 50, 'A7', 'N'),
+('UBI-E3-001', 'AAAAA', 50, 'E3', 'N'),
+('UBI-E3-002', 'BBBBB', 50, 'E3', 'N'),
+('UBI-E3-003', 'CCCCC', 50, 'E3', 'N'),
+('UBI-E4-001', 'AAAAA', 50, 'E4', 'N'),
+('UBI-E4-002', 'BBBBB', 50, 'E4', 'N'),
+('UBI-E4-003', 'CCCCC', 50, 'E4', 'N'),
+('UBI-M1-001', 'AAAAA', 50, 'M1', 'N'),
+('UBI-M1-002', 'BBBBB', 50, 'M1', 'N'),
+('UBI-M1-003', 'CCCCC', 50, 'M1', 'N'),
+('UBI-M2-001', 'AAAAA', 50, 'M2', 'N'),
+('UBI-M2-002', 'BBBBB', 50, 'M2', 'N'),
+('UBI-M2-003', 'CCCCC', 50, 'M2', 'N'),
+('UBI-M4-001', 'AAAAA', 50, 'M4', 'N'),
+('UBI-M4-002', 'BBBBB', 50, 'M4', 'N'),
+('UBI-M4-003', 'CCCCC', 50, 'M4', 'N');
 
 --
 -- Indici per le tabelle scaricate
