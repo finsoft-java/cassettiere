b7170c4d8edf0f779dc125011af04f9ad8300916
Luca Vercelli <luca.vercelli.to@gmail.com>
2021-04-09 09:42:40 +0200

    eslint

--- cassettiere/src/app/ubicazioni/ubicazioni.component.ts
+++ cassettiere/src/app/ubicazioni/ubicazioni.component.ts
@@ -1,18 +1,16 @@
-import { FormControl } from '@angular/forms';
-import { AlertService } from './../_services/alert.service';
-import { MatTableDataSource } from '@angular/material/table';
-import { AreeService } from './../_services/aree.service';
-import { ColumnDefinition, LabelValue } from './../mat-edit-table/ColumnDefinition';
-import { UbicazioniService } from './../_services/ubicazioni.service';
+import { map } from 'rxjs/operators';
 import { Component, OnInit } from '@angular/core';
+import { AlertService } from '../_services/alert.service';
+import { ColumnDefinition, LabelValue } from '../mat-edit-table/ColumnDefinition';
+import { UbicazioniService } from '../_services/ubicazioni.service';
 import { Ubicazione } from '../_models';
 import { ArticoliService } from '../_services/articoli.service';
-import { map } from 'rxjs/operators';
+import { AreeService } from '../_services/aree.service';
 
 @Component({
   selector: 'app-ubicazioni',
   templateUrl: './ubicazioni.component.html',
-  styleUrls: ['./ubicazioni.component.css']
+  styleUrls: ['./ubicazioni.component.css'],
 })
 export class UbicazioniComponent implements OnInit {
   arrayAree: LabelValue[] = [];
@@ -31,15 +29,13 @@ export class UbicazioniComponent implements OnInit {
       title: 'Cod. Articolo',
       data: 'COD_ARTICOLO_CONTENUTO',
       type: 'combo',
-      asyncOptions: (row) => {
-        return this.articoliService.getAll({ top: 15, search: row?.COD_ARTICOLO_CONTENUTO })
-          .pipe(map(listBean => listBean.data.map(x => {
-              return {
-                label: x.ID_ARTICOLO + ' - ' + x.DESCRIZIONE,
-                value: x.ID_ARTICOLO
-              };
-            })));
-      },
+      asyncOptions: (row) => this.articoliService.getAll({ top: 15, search: row?.COD_ARTICOLO_CONTENUTO })
+        .pipe(map(listBean => listBean.data.map(x => (
+          {
+            label: x.ID_ARTICOLO + ' - ' + x.DESCRIZIONE,
+            value: x.ID_ARTICOLO
+          }
+        )))),
       width: '20%'
     },
     {
@@ -59,7 +55,7 @@ export class UbicazioniComponent implements OnInit {
       title: 'In esaurimento',
       data: 'SEGNALAZIONE_ESAURIMENTO',
       type: 'select',
-      options: [{label: 'Sì', value: 'Y'}, {label: 'No', value: 'N'}],
+      options: [{ label: 'Sì', value: 'Y' }, { label: 'No', value: 'N' }],
       render: (data) => (data === 'N' ? 'No' : 'Sì'),
       width: '10%'
     }
@@ -72,28 +68,27 @@ export class UbicazioniComponent implements OnInit {
     this.service = ubicSvc;
   }
 
-  ngOnInit(): void {
-    // tslint:disable-next-line: deprecation
+  ngOnInit() {
     this.areeSvc.getAll().subscribe(
       response => {
         for (let i = 0; i < response.data.length; i++) {
-          this.arrayAree.push({label: response.data[i].COD_AREA + ' - ' + response.data[i].DESCRIZIONE,
-          value: response.data[i].COD_AREA});
+          this.arrayAree.push({
+            label: response.data[i].COD_AREA + ' - ' + response.data[i].DESCRIZIONE,
+            value: response.data[i].COD_AREA
+          });
         }
       },
       error => {
       }
     );
-    this.arrayEsaurimento.push({label: 'No', value: 'N'}, {label: 'Si', value: 'Y'});
+    this.arrayEsaurimento.push({ label: 'No', value: 'N' }, { label: 'Si', value: 'Y' });
   }
 
-  filterRow(editTableComponent: any): void {
-
+  filterRow(editTableComponent: any) {
     if (this.filter.search) {
       this.filter.search = this.filter.search.trim();
     }
     editTableComponent.filter(this.filter);
-
   }
 
   resetFilter(editTableComponent: any): void {
