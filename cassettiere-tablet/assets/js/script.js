53304eafce207948ef87e42419d18da0c77d0b18
Alessandro <alessandrobarsanti6@gmail.com>
2021-04-12 11:59:08 +0200

    fix

--- cassettiere-tablet/assets/js/script.js
+++ cassettiere-tablet/assets/js/script.js
@@ -1,17 +1,14 @@
-var tokenString = 'pippo';
-
 $(document).on("click","#login", function(){
     alert(1);
     login();
 });
 
 $(document).on("click","#ablLettore", function(){
-    $("#rfid").val("finsoft");
+    $("#rfid").val("1234");
     $("#login").css("display","");
 });
 
 function login (){
-    alert(2);
     rfid = $("#rfid").val();
     $.post({
     url: "../../cassettiere/ws/LoginBadge.php",
@@ -20,11 +17,9 @@ function login (){
         rfid: rfid
     },
     success: function(data, status) {
-        alert();
       $("#rfid").val("");
       $("#login").css("display","none");
-      console.log("The returned data", data);
+      console.log(data);
     }
-    ,beforeSend: function(xhr, settings) { xhr.setRequestHeader('Authorization','Bearer ' + tokenString ); } //set tokenString before send
   });
 }
