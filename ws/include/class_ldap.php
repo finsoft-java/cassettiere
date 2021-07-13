6ac68034ffdfb0bc47b55c751f4549186c7b5c45
Luca Vercelli <luca.vercelli.to@gmail.com>
2021-07-13 09:20:15 +0200

    gestione LDAP

--- ws/include/class_ldap.php
+++ ws/include/class_ldap.php
@@ -4,40 +4,78 @@ $ldapManager = new LdapManager();
 
 class LdapManager {
     
-    function get_user($username) {
+    /**
+     * Login dell'utente con ruolo magazziniere (serve per il backend)
+     */
+    function login($username, $pwd) {
+        return $this->_common_get_user(AD_SERVER, $username . '@' . AD_DOMAIN, $pwd, $username, AD_BASE_DN, [AD_FILTER => 'magazziniere']);
+    }
 
-        $ldap_connection = ldap_connect(AD_SERVER);
+    /**
+     * Ricerca dell'utente con ruolo magazziniere oppure utente semplice (serve per il frontend)
+     */
+    function get_user($username) {
+        if ($useFilter) {
+            $additionalFilter = AD_FILTER;
+        } else {
+            $additionalFilter = null;
+        }
+        return $this->_common_get_user(AD_SERVER, AD_USERNAME, AD_PASSWORD, $username, AD_BASE_DN, [AD_FILTER => 'magazziniere', '' => 'utente']);
+    }
+
+    /**
+     * Si connette a AD con le credenziali date. Cerca lo $username dato e si aspetta di trovare un unico record.
+     * $mapFiltersRoles serve per effettuare più ricerche, una per ogni $ruolo desiderato, verrà assegnato il primo che trova.
+     * Se non trova nulla dà errore 404.
+     */
+    function _common_get_user($server, $login_user, $login_pwd, $username, $searchBase, $mapFiltersRoles) {
+
+        $stoFacendoLogin = ($login_user === $username . '@' . AD_DOMAIN);
+
+        $ldap_connection = ldap_connect($server);
         if (FALSE === $ldap_connection) {
-            print_error(500, "Errore interno nella configurazione di Active Directory: " . AD_SERVER);
+            print_error(503, "Errore interno - Impossibile collegarsi al sever Active Directory: " . AD_SERVER);
         }
 
         // We have to set this option for the version of Active Directory we are using.
         ldap_set_option($ldap_connection, LDAP_OPT_PROTOCOL_VERSION, 3) or die('Unable to set LDAP protocol version');
         ldap_set_option($ldap_connection, LDAP_OPT_REFERRALS, 0); // We need this for doing an LDAP search.
 
-        ini_set('display_errors', 0);  // Se ci sono errori, ldap_bind() scrive sullo stdout e fa casino....
-        $bind = ldap_bind($ldap_connection, AD_USERNAME, AD_PASSWORD);
+        $bind = @ldap_bind($ldap_connection, $login_user, $login_pwd);
         if ($bind !== TRUE) {
-            print_error(502, "Impossibile connettersi al server Active Directory");
+            if ($stoFacendoLogin) {
+                print_error(403, 'Credenziali invalide: ' . $username);
+            } else {
+                print_error(500, "Errore interno - Impossibile accedere al server Active Directory con l'utenza amministrativa");
+            }
         }
 
-        $ldap_base_dn = AD_BASE_DN;
-        $search_filter = "(&(|(objectCategory=person)(objectClass=user))(sAMAccountName=$username))";
-        $attributes = array();
-        $attributes[] = 'givenname';
-        $attributes[] = 'mail';
-        $attributes[] = 'samaccountname';
-        $attributes[] = 'sn';
-        $result = ldap_search($ldap_connection, $ldap_base_dn, $search_filter, $attributes);
-        
-        $user = null;
-
-        if (FALSE !== $result){
-            $entries = ldap_get_entries($ldap_connection, $result);
+        foreach ($mapFiltersRoles as $additionalFilter => $role) {
+            if ($additionalFilter) {
+                $searchFilter="(&(SamAccountName=$username)$additionalFilter)";
+            } else {
+                $searchFilter="(SamAccountName=$username)";
+            }
+            $attributes = array();
+            $attributes[] = 'givenname';
+            $attributes[] = 'mail';
+            $attributes[] = 'samaccountname';
+            $attributes[] = 'sn';
+            $result = ldap_search($ldap_connection, $searchBase, $searchFilter, $attributes);
             
+            if (FALSE === $result) {
+                continue;
+            }
+
+            $entries = ldap_get_entries($ldap_connection, $result);
+
+            if ($entries['count'] == 0) {
+                continue;
+            }
+
             // should be exactly 1
             if ($entries['count'] != 1) {
-                ldap_unbind($ldap_connection); // Clean up after ourselves.
+                @ldap_unbind($ldap_connection); // Clean up after ourselves.
                 print_error(500, 'More than 1 user found for username ' . $username);
             }
 
@@ -47,16 +85,20 @@ class LdapManager {
                 'nome' => $entry['givenname'][0],
                 'cognome' => $entry['sn'][0],
                 'email' => $entry['mail'][0],
-                'ruolo' => 'magazziniere' // FIXME
+                'ruolo' => $role
             ];
-        }
-        ldap_unbind($ldap_connection); // Clean up after ourselves.
 
-        if ($user == null) {
-            print_error(404, 'Username not recognized: ' . $username);
+            @ldap_unbind($ldap_connection); // Clean up after ourselves.
+            return $user;
         }
 
-        return $user;
+        @ldap_unbind($ldap_connection); // Clean up after ourselves.
+        
+        if ($stoFacendoLogin) {
+            print_error(403, 'Utente non autorizzato: ' . $username);
+        } else {
+            print_error(403, 'Utente inesistente o non autorizzato: ' . $username);
+        }
     }
 }
 ?>
--- ws/include/config.php
+++ ws/include/config.php
@@ -17,9 +17,12 @@ define('JWT_SECRET_KEY', 'OSAISECRET2021');
 /* LDAP / ACTIVE DIRECTORY */
 define('AD_SERVER', 'ldap://osai.loc');
 define('AD_DOMAIN', 'OSAI.LOC');
-define('AD_BASE_DN', "dc=OSAI,dc=LOC");
-define('AD_FILTER', '(objectclass=person)');
-// define('AD_FILTER', '(&(|(objectclass=person))(|(|(memberof=CN=OSAI-IT Users,OU=OU Osai Groups,DC=osai,DC=loc)(primaryGroupID=1202))(|(memberof=CN=OSAI-DE Users,OU=OU Osai Groups,DC=osai,DC=loc)(primaryGroupID=2625))(|(memberof=CN=OSAI-CN Users,OU=OU Osai Groups,DC=osai,DC=loc)(primaryGroupID=3233))(|(memberof=CN=OSAI-US Users,OU=OU Osai Groups,DC=osai,DC=loc)(primaryGroupID=4426))))');
+// define('AD_BASE_DN', "dc=OSAI,dc=LOC");
+define('AD_BASE_DN', "OU=Users,OU=OU Osai.it,DC=osai,DC=loc");
+// AD_FILTER: è il gruppo dei "Magazzinieri"
+// Solo i magazzinieri possono accedere al backend.
+// Invece al frontend possono accedere tutti gli utenti con badge, tuttavia i magazzinieri hanno più privilegi.
+define('AD_FILTER', '(memberOf=CN=Gruppo Logistica,OU=OU Osai Groups,DC=osai,DC=loc)');
 define('AD_USERNAME', 'surveyosai@OSAI.LOC');
 define('AD_PASSWORD', 's0fu3Y2o19!');
 
