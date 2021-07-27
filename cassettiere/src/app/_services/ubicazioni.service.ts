e217ecd3a68fe0b2d55f38844a63fa29ad6f5ac8
Luca Vercelli <luca.vercelli@it-present.com>
2021-07-27 09:25:19 +0200

    clean

--- cassettiere/src/app/_services/ubicazioni.service.ts
+++ cassettiere/src/app/_services/ubicazioni.service.ts
@@ -12,82 +12,20 @@ import { environment } from 'src/environments/environment';
 export class UbicazioniService implements HttpCrudService<Ubicazione> {
 
   constructor(private http: HttpClient) { }
-  isMock = true;
 
-  mockData: ListBean<Articolo> = {
-    data: [
-      {
-        ID_ARTICOLO: '1',
-        DESCRIZIONE: 'PRODOTTO M1'
-      },
-      {
-        ID_ARTICOLO: '2',
-        DESCRIZIONE: 'PRODOTTO M2'
-      },
-      {
-        ID_ARTICOLO: '3',
-        DESCRIZIONE: 'PRODOTTO M3'
-      },
-      {
-        ID_ARTICOLO: '4',
-        DESCRIZIONE: 'PRODOTTO M4'
-      },
-      {
-        ID_ARTICOLO: '5',
-        DESCRIZIONE: 'PRODOTTO M5'
-      }
-    ],
-    count: 5
-  };
-
-  getAll(filter?: any): Observable<ListBean<any>> {
-    this.isMock = false;
-    if (this.isMock) {
-      return new Observable( observer => {
-        // JSON parse/stringify serve per eseguire una deep copy
-        const list: ListBean<any> = JSON.parse(JSON.stringify(this.mockData));
-        observer.next(list);
-        observer.complete();
-      });
-    } else {
-      return this.http.get<ListBean<any>>(environment.wsUrl + 'Ubicazioni.php', { params: filter || {}});
-    }
+  getAll(filter?: any): Observable<ListBean<Ubicazione>> {
+    return this.http.get<ListBean<Ubicazione>>(environment.wsUrl + 'Ubicazioni.php', { params: filter || {}});
   }
 
-  create(obj: any): Observable<ValueBean<any>> {
-    this.isMock = false;
-    if (this.isMock) {
-      return new Observable( observer => {
-        observer.next({ value: obj });
-        observer.complete();
-      });
-    } else {
-      return this.http.post<ValueBean<any>>(environment.wsUrl + 'Ubicazioni.php', obj);
-    }
+  create(obj: any): Observable<ValueBean<Ubicazione>> {
+    return this.http.post<ValueBean<Ubicazione>>(environment.wsUrl + 'Ubicazioni.php', obj);
   }
 
-  update(obj: any): Observable<ValueBean<any>> {
-    this.isMock = false;
-    if (this.isMock) {
-      return new Observable( observer => {
-        observer.next({ value: obj });
-        observer.complete();
-      });
-    } else {
-      return this.http.put<ValueBean<any>>(environment.wsUrl + 'Ubicazioni.php', obj);
-    }
+  update(obj: any): Observable<ValueBean<Ubicazione>> {
+    return this.http.put<ValueBean<Ubicazione>>(environment.wsUrl + 'Ubicazioni.php', obj);
   }
 
   delete(obj: any): Observable<void> {
-    console.log(obj);
-    this.isMock = false;
-    if (this.isMock) {
-      return new Observable( observer => {
-        observer.next(undefined);
-        observer.complete();
-      });
-    } else {
-      return this.http.delete<any>(environment.wsUrl + `Ubicazioni.php?COD_UBICAZIONE=${obj.COD_UBICAZIONE}`);
-    }
+    return this.http.delete<void>(environment.wsUrl + `Ubicazioni.php?COD_UBICAZIONE=${obj.COD_UBICAZIONE}`);
   }
 }
